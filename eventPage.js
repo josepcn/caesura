f6c873d713a60d12feafb608c72eb52593784bab
Josep <josep@jmacbook.home>
2015-04-09 21:44:43 +0200

    first draft of rebuilding event page

--- eventPage.js
+++ eventPage.js
@@ -1,46 +1,85 @@
 
-function sendSwitchRequestToAllTabs(){
-	chrome.tabs.query({}, 
-		function(tabs){
-    		for (var i = 0; i < tabs.length; i++) {
-				sendSwitchRequestToTab(tabs[i].id)                 
-    		}
-		});
-};
-
-function registerLastTabThatPausedMusic( tabId ){
-	alert("storing tab" + tabID)
-	chrome.storage.local.set({'lastPauseTabId': tabId})
-}
-
-function switchAudioMessageResponseCallback( response, tabId ){
-	if( response ){
-		if( response.paused ){
-			registerLastTabThatPausedMusic( tabId )
-		}
-	}
-}
-
-function sendSwitchRequestToTab( tabId ){
+/*
+function sendPauseMessageToTab( tabId ){
 	chrome.tabs.sendMessage( tabId, 
-							 {action: "switchAudio"},
+							 {action: 'pauseMusic'},
 							 function(r){
 							 	switchAudioMessageResponseCallback(r, tabId)
 							 })
 }
+*/
 
+function sendPauseMessageToTab( tabId ){
+	chrome.tabs.sendMessage( tabId, {action: 'pauseMusic'} )
+}
+
+function sendPlayMessageToTab( tabId ){
+	chrome.tabs.sendMessage( tabId, {action: 'playMusic'} )
+}
+
+function sendIsPlayingMessageToTab( tabId ){
+	responseIsPlaying = false
+	chrome.tabs.sendMessage( tabId, 
+							 {action: 'notifyIsPlaying'},
+							 function(r){
+							 	if( response ){
+									if( response.isPlaying == true ){
+										responseIsPlaying = true
+									}
+								}
+							 })
+	return responseIsPlaying
+}
+
+
+function getTabsPlaying(){
+	tabsPlaying = []
+
+	chrome.tabs.query({}, 
+		function(tabs){
+    		for (var i = 0; i < tabs.length; i++) {
+				isPlaying = sendIsPlayingMessageToTab(tabs[i].id)  
+				if( isPlaying ){
+					tabsPlaying.push(tabs[i].id)
+				}              
+    		}
+		});
+	return tabsPlaying
+}
+
+
+function getLastPausedTabFromStore(){
+	tabID = -1
+
+	chrome.storage.local.get('lastPauseTabId', 
+			function(items){
+				tabID = items['lastPauseTabId']
+			})
+
+	return tabID
+}
 
 chrome.commands.onCommand.addListener(function(command) {
 	if (command == "toggle-audio-playback") {
-		// found if there is a tab stored that paused the music
-		chrome.storage.local.get('lastPauseTabId', 
-			function(items){
-				tabID = items['lastPauseTabId']
-				//alert("tab id found " + tabID)
+		tabsPlaying = getTabsPlaying()
 
-			})
+		if( tabsPlaying.length > 0 ){
+			// some tabs are playing music, let's pause them
+			for( tabID in tabsPlaying ){
+				sendPauseMessageToTab(tabID)
+			}
+			// store just one for now
+			chrome.storage.local.set({'lastPauseTabId': tabId})
+		}
+		else{
+			// no tabs playing, try to resume 
+			lastTabPausedID = getLastPausedTabFromStore()
+			if( lastTabPausedID != -1 ){
+				sendPlayMessageToTab( lastTabPausedID )
+			}
 
-		sendSwitchRequestToAllTabs()
+
+		}
     }
 })
 
