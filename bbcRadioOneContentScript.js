bcbddafe95805ec8541120ecf1e6f542575edba8
Josep <josep@JMacBook.local>
2015-04-10 19:52:19 +0200

    cleaning

--- bbcRadioOneContentScript.js
+++ bbcRadioOneContentScript.js
@@ -65,28 +65,4 @@ function isMusicPlaying(){
 	return playing
 }
 
-/*
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-    	if (request.action == 'notifyIsPlaying'){
-    		playing = isMusicPlaying()
-    		sendResponse({isPlaying: playing})
-    	}
-	});
 
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-    	if (request.action == 'playMusic'){
-    		ok = play()
-    		sendResponse({couldPlay: ok})
-    	}
-	});
-
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-    	if (request.action == 'pauseMusic'){
-    		ok = pause()
-    		sendResponse({couldPause: ok})
-    	}
-	});
-*/
