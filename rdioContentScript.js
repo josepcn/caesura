435421853ace04634397f845e3fc1d19c6873e60
Josep <josep@jmacbook.home>
2015-04-14 20:33:47 +0200

    rdio, soundcloud, deezer scripts only define the classes

--- rdioContentScript.js
+++ rdioContentScript.js
@@ -2,32 +2,3 @@
 var notPlayingClassName = "play_pause"
 var playingClassName = "play_pause playing"
 
-function playMusic(){
-	var ok = false
-
-	var notPlayingElements = document.getElementsByClassName(notPlayingClassName);
-	if( notPlayingElements.length == 1 ){
-		notPlayingElements[0].click()
-		ok = true
-	}
-	return ok
-}
-
-function pauseMusic(){
-	var ok = false
-
-	var playingElements = document.getElementsByClassName(playingClassName);
-	if( playingElements.length == 1 ){
-		playingElements[0].click()
-		ok = true
-	}
-	return ok
-}
-function isMusicPlaying(){
-	var playing = false
-	var playingElements = document.getElementsByClassName(playingClassName);
-	if( playingElements.length == 1 ){
-		playing = true
-	}
-	return playing
-}
